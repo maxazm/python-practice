788dffb48eb807fcf0fda9e839c27d0f21d9eae9
maxazm <zomk0222@gmail.com>
2022-01-29 17:28:24 +0900

    list

--- basic/in_operator.py
+++ basic/in_operator.py
@@ -6,8 +6,10 @@ print("lemon" not in fruits)
 #challenge
 favorite_fruits = input("好きなフルーツを入力してください : ")
 if favorite_fruits in fruits:
+    print(f"{favorite_fruits}ですね。差し上げます")
     fruits.remove(favorite_fruits)
     print(fruits)
 elif favorite_fruits not in fruits:
+    print(f"{favorite_fruits}ですね。仕入れました" )
     fruits.append(favorite_fruits)
     print(fruits)
