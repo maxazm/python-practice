4755eed5061bb57832842b7904a7923f9ac21f8e
maxazm <zomk0222@gmail.com>
2022-01-30 23:34:56 +0900

    module practice

---
+++ module/time_practice.py
@@ -0,0 +1,49 @@
+from functools import lru_cache
+import time
+# .time(): 1970/1/1　秒数が表示 unix時間
+print(time.time())
+
+@lru_cache #cacheを保持する
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+before = time.time()
+fibonacci(30)
+print(fibonacci(30))
+after = time.time()
+print(f"recursive fibonacci took {after - before:.2f} sec.")
+
+# .ctime() 今のローカル時間を文字列で返す
+print(time.ctime())
+# .localtime() 構造化データで返す
+localtime = time.localtime()
+print(localtime)
+print("今の時刻は{0.tm_year}年{0.tm_mon}月{0.tm_mday}日{0.tm_hour}時{0.tm_min}分です".format(localtime))
+
+#.sleep(secs) secs秒だけプログラムが待機する
+sec = 10
+print(f"{sec}秒だけ待ってください")
+# time.sleep(sec)
+print(f"{sec}秒経ちました")
+
+#challenge
+def count_timer(func):
+
+    def inner(*args, **kwargs):
+        before = time.time()
+        func(*args, **kwargs)
+        after = time.time()
+        print(f"{func.__name__} took {after - before:.2f}sec ")
+    return inner
+
+
+@count_timer
+def calculate(x, y):
+    print("計算します")
+    time.sleep(4)
+    print(f"計算結果は{x * y}です")
+
+calculate(3,1)
