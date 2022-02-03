bb470523badc794f2a21e327d3b77c6d9f0f6050
maxazm <zomk0222@gmail.com>
2022-02-03 15:23:00 +0900

    python style practice

---
+++ style_guide/check_tool.py
@@ -0,0 +1,4 @@
+import os, sys
+x =1
+y =1
+print             (x,y)
--- style_guide/style_guide.py
+++ style_guide/style_guide.py
@@ -1,5 +1,7 @@
 #変数定義
 #correct
+import math
+
 x = 1
 y = 2
 
@@ -43,3 +45,85 @@ foo = long_function_name(
     var_one, var_two,
     var_three, var_four
 )
+
+#correct
+def long_function_name(
+        var_one, var_two, var_three, var_four
+)
+# \で区切って改行する
+print("このように表示する文章が長かったりする場合は\
+      バックスラッシュで区切ると改行できます")
+
+a = 100000000000000000 \
+    + 10000 \
+    + 1000 \
+    + 121 \
+    + 100000
+
+#関数間は二行あける
+def func():
+    pass
+
+
+def func2():
+    pass
+
+#class内は一行あける
+class MyClass:
+    def __init__(self):
+        pass
+
+    def func(self):
+        pass
+
+#importのstyle
+import os
+import sys
+
+
+#wrong
+import os, sys
+
+from subprocess import Popen, PIPE
+
+#順番
+#1. standard library (time, sys)
+#2. Third Party (numpy, pandas)
+#3. Our library
+#4. local library
+#それぞれ一行あける abc順
+
+#absolute import
+import mypkg.sibling
+
+# a = 1
+# のあとは半角スペースあける
+a = 1  # インラインコメント 2半角スペース空ける
+
+# - _xxx: internal use only(non public)
+# - xxx_: pythonですでに使われている単語を使いたいとき　ex type_
+# - __xxx: クラスの属性として使うことで名前修飾される
+# -__xxx__: magic methodと呼ばれるもので　pythonが特別に設定しているもの
+# - _: 最近実行した戻り値や、iteration時に使わない変数
+
+for _ in range(18):
+    print("hello")
+
+#return
+def foo(x):
+    if x >= 0:
+        return math.sqrt(x)
+    else:
+        return None
+
+#オブジェクトタイプの確認はisinstance()を使う
+if isinstance(obj, int)
+
+#wrong
+type(obj) is type(1)
+
+def greeting(name: str) -> str:
+    return "Hello" + name
+#一つでもannotationをつけたら全てにつける
+#pythonがtypeのチェックをしてくれるわけではない
+#pythonは動的片付け言語であることを意識
